c1065902009ee62b355a5fc46d2f3d4b02bdff53
jervygu <jervygu@gmail.com>
2020-06-11 10:50:40 +0800

    Finished Calculator Auto layout

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Jaypee Umandap on 6/11/20.
+//  Copyright © 2020 Jervy Umandap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
